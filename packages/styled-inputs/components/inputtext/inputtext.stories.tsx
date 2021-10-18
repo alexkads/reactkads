5cf121d6c11bfebc2ecb32eb5ad385d2abb8489e
Alex S S Fonseca <alexkads@gmail.com>
2021-10-18 19:49:56 -0300

    Atualizado Storybook

---
+++ packages/styled-inputs/components/inputtext/inputtext.stories.tsx
@@ -0,0 +1,60 @@
+import React, { ComponentProps } from "react";
+import { Story, Meta, storiesOf } from "@storybook/react";
+import { InputText } from ".";
+import { useState } from "@storybook/addons";
+
+storiesOf("InputText", module).add("Login", () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    if (name === "username") setUsername(value);
+    if (name === "password") setPassword(value);
+  };
+
+  return (
+    <div style={{ width: "350px", backgroundColor: "grey", padding: "40px" }}>
+      <InputText
+        label="Nome do Usuário"
+        name="username"
+        onChange={handleOnChange}
+        value={username}
+      />
+
+      <InputText
+        type="password"
+        label="Senha"
+        name="password"
+        onChange={handleOnChange}
+        value={password}
+      />
+    </div>
+  );
+});
+
+export default {
+  title: "InputText",
+  component: InputText,
+  parameters: {
+    actions: {
+      handles: ["", "click .btn"],
+    },
+  },
+} as Meta;
+
+const Template: Story<ComponentProps<typeof InputText>> = args => (
+  <div style={{ width: "350px", backgroundColor: "grey", padding: "40px" }}>
+    <InputText {...args} />
+    <InputText {...args} />
+  </div>
+);
+
+export const FirstStory = Template.bind({});
+
+FirstStory.args = {
+  label: "Nome",
+  name: "teste",
+  value: "teste",
+};
