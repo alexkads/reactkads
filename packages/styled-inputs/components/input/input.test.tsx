6e38043bace2bb7f30a953160138cd3ff6be9383
Alex S S Fonseca <alexkads@gmail.com>
2021-10-18 12:00:46 -0300

    Melhorias no Teste

--- packages/styled-inputs/components/input/input.test.tsx
+++ packages/styled-inputs/components/input/input.test.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { Button } from ".";
+import { Input } from ".";
 
 test("should render with default values", () => {
-  const { debug } = render(<Button>Texto</Button>);
-  debug();
-  const button = screen.getByRole("button", { name: "Texto" });
-  expect(button);
+  render(<Input type="email" />);
 });
